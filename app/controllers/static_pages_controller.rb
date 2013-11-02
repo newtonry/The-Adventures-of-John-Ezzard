ccee395f01a83e4069f1bcedae9b3b3e025a0ee8
newtonry <vonkunesnewton@gmail.com>
2013-11-02 15:46:43 -0700

    Added some styling

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,6 @@
+class StaticPagesController < ApplicationController
+  def about
+    render :about
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ JohnEzzard::Application.routes.draw do
   resources :stories
   
   root "stories#index"
+  match "/about", to: "static_pages#about", via: 'get'
 
 end
